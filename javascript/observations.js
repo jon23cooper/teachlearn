bd099cde0f328108f1a28dd06eebf9cc650588ea
Jon <jcooper.ffh@gmail.com>
2015-09-02 07:11:20 +0100

    Observations page added
    Observations page displays list of all observations

--- javascript/observations.js
+++ javascript/observations.js
@@ -1,3 +1,29 @@
 if (Meteor.isClient){
+	
+	
+	
+	Template.observations.onCreated(function () {
+		this.subscribe('teachers');
+		
+	});
+	
+	Template.observations.helpers({
+		observations: function(sortBy){
+			var teachersArray = Teachers.find({},{$sort:{_id: 1}}).fetch();
+			var observations =[];
+			teachersArray.forEach(function(teacher, idx){
+				teacher.observations.forEach(function(obs, i){
+					observations.push({"teacher": teacher._id, "obs": obs});
+				});
+			});
+			return observations;
+		},
+		
+		sortByDate: function(){
+			observations.sort(function(a, b){
+				return new Date(b.date) - new Date(a.date);
+			})
+		},
 
+	});
 }
