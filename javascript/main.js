d75823b2948108d1604e13902ec99091239aab1c
jon23cooper <jcooper.ffh@gmail.com>
2015-09-03 08:04:21 +0000

    file naming and placement adjustments

---
+++ javascript/main.js
@@ -0,0 +1,13 @@
+If (Meteor.isClient){
+
+  Template.main.helpers({
+
+    });
+
+    Template.main.events({
+      "click #teacherBtn": function(event){
+        event.preventDefault();
+        Router.go('teachers');
+      }
+    });
+}
